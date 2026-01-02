0a50f37c802e74d615495aaca8bb5bf39a7456d5
AbhineshJha <abhinesh_223002@saitm.org>
2026-01-02 19:17:47 +0530

    Moved the user creation feature in auth part

---
+++ src/main/java/com/opencode/alumxbackend/auth/dto/RegisterRequest.java
@@ -0,0 +1,36 @@
+package com.opencode.alumxbackend.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    @NotBlank(message = "Username is required")
+    @Size(max = 12, message = "Username must be 12 characters or less")
+    private String username;
+
+    @NotBlank(message = "Full name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Role is required (STUDENT/ALUMNI/PROFESSOR)")
+    private String role;
+}
